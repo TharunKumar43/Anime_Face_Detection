88104b0b34517550a894de668fe870ee6e89a23a
Tharunkumar <146437885+TharunKumar43@users.noreply.github.com>
2025-02-14 14:19:33 +0530

    Add files via upload

---
+++ mainpro.py
@@ -0,0 +1,27 @@
+import os
+from roboflow import Roboflow
+
+if not os.path.exists('output'):
+    os.mkdir('output')
+
+rf = Roboflow(api_key="oeNDFzKWWDyLjxNf0Cmp")
+project = rf.workspace().project("animefaces")
+model = project.version(2).model
+
+input_folder = "D:/Projects/Anime_Face_Detection/input"
+output_folder = "D:/Projects/Anime_Face_Detection/output"
+confidence_threshold = 40
+overlap_threshold = 30
+  
+input_files = os.listdir(input_folder)
+
+for input_file in input_files:
+    input_path = os.path.join(input_folder, input_file)
+    output_path = os.path.join(output_folder, f"prediction_{input_file}")
+
+    print(model.predict(input_path, confidence=confidence_threshold, overlap=overlap_threshold).json())
+
+    prediction = model.predict(input_path, confidence=confidence_threshold, overlap=overlap_threshold)
+
+    prediction.save(output_path)
+    print(input_file)
